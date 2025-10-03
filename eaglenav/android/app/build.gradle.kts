8cf1887b2c683a0aad247334f35fd55e09aa69c3
Shadowslayz <jian1mejia@gmail.com>
2025-10-02 23:22:05 -0700

    Created Skeleton App

--- eaglenav/android/app/build.gradle.kts
+++ eaglenav/android/app/build.gradle.kts
@@ -7,37 +7,34 @@ plugins {
 
 android {
     namespace = "com.example.eaglenav"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36   // Force latest SDK
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17   // Bump to 17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.eaglenav"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion    // Or higher if plugin docs require
+        targetSdk = 36 // Force latest
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
+
 
 flutter {
     source = "../.."

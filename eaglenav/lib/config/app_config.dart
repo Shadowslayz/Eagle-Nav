459c94253d100568a7f07faad43dc3d71e0d17b4
Abigail-diaz <abigailsolano128@gmail.com>
2025-11-05 06:16:15 -0800

    update: Cleaned up config file

--- eaglenav/lib/config/app_config.dart
+++ eaglenav/lib/config/app_config.dart
@@ -1,15 +1,16 @@
-// lib/config.dart
-// app configuration for Valhalla server URLs
-import 'dart:io' show Platform;
+import 'dart:io';
 
 class AppConfig {
-  // Set this true if running on a real phone/tablet hitting your Mac over Wi-Fi.
-  static const bool useLanIp = false;
-  static const String lanBase = 'http://192.168.1.123:8002'; // <- your Mac's IP
-
   static String get valhallaBaseUrl {
-    if (Platform.isAndroid) return 'http://10.0.2.2:8002';
-    //if (Platform.isAndroid) return 'http://localhost:8002';
-    return 'http://localhost:8002'; // iOS Simulator / macOS
+    if (Platform.isAndroid) {
+      // If Android emulator → 10.0.2.2 is always the HOST MACHINE
+      return 'http://10.0.2.2:8002';
+    }
+    if (Platform.isIOS) {
+      // iOS Simulator uses localhost directly
+      return 'http://localhost:8002';
+    }
+
+    return 'http://10.0.2.2:8002';
   }
 }

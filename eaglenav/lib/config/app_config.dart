1df5ec663d8e78ef12dc118e652a57ed175ff24a
Abigail Diaz <123652655+Abigail-Diaz@users.noreply.github.com>
2026-03-09 18:50:44 -0700

    feat(deployment): added a command to deploy on a physical phone

--- eaglenav/lib/config/app_config.dart
+++ eaglenav/lib/config/app_config.dart
@@ -1,16 +1,22 @@
 import 'dart:io';
 
 class AppConfig {
+  static const String _localNetworkIp = ''; // put your local IP address here
+  static const bool _isPhysicalDevice = bool.fromEnvironment(
+    'PHYSICAL_DEVICE',
+    defaultValue: false,
+  );
+
   static String get valhallaBaseUrl {
+    if (_isPhysicalDevice) {
+      return 'http://$_localNetworkIp:8002';
+    }
     if (Platform.isAndroid) {
-      // If Android emulator → 10.0.2.2 is always the HOST MACHINE
       return 'http://10.0.2.2:8002';
     }
     if (Platform.isIOS) {
-      // iOS Simulator uses localhost directly
       return 'http://localhost:8002';
     }
-
     return 'http://10.0.2.2:8002';
   }
 }
